8788200a33600ed67f479711c200fec8865230d0
NullumVerbum <nullumverbum@kali.kali.local>
2025-12-10 23:11:08 +0100

    Add command mode with direct shell execution
    Features:
    - New commandMode() accessible via 'CM!' or 'command mode'
    - Support CMD prefix in main menu for inline commands (e.g., 'CMD ls')
    - Colored prompts using ANSI escape codes
    - Display current working directory in interfaces
    
    Refactoring:
    - Rename userInput() to mainMenu() for clarity
    - Move system info display into mainMenu
    - Improve function organization and flow
    - Add clear/cls command support across interfaces

--- sysinfo.sh
+++ sysinfo.sh
@@ -4,32 +4,34 @@
 BLUE="\e[34m"
 RESET="\e[0m"
 GREY="\e[90m"
-RED="\e["
+RED="\e[31m"
 
 # User interface for monitroring and managing users on the system
 userInterface() {
 	clear
 	showheader
-	lslogins -u 
-	echo 
-	who
-	echo
+	echo -e "${GREY}Current user on system: ${RESET}\n$(lslogins -u) ${GREY}\nCurrently logged in: \n${RESET}$(who)\n$(pwd)"
+	echo -e "${BLUE}++++++++++++++++++++++++++++ ${RESET}"
 	
 	while true; do
-		read -p "$(whoami) >" user_input
+		read -p "$(whoami):UI > " user_input
 
 		case "$user_input" in
 			"list all"|list|la)
 			lslogins
 			;;
 			exit|quit|"main menu")
-			userInput
+			mainMenu
 			;;
 			"exit!"|"quit!")
 			exit
 			;;
+			clear|cls)
+			clear
+			userInterface
+			;;
 			*)
-			echo -e "Unkown command. Try \"help\" to see all available commands.  "
+			echo -e "${RED}Unkown command. Try \"help\" to see all available commands.${RESET}"
 			;;
 		esac
 	done
@@ -40,7 +42,7 @@ hardwareInterface() {
 	clear
 	showheader
 	echo "Hardware Interface"
-	userInput
+	mainMenu
 }
 
 # FM interface for intuitively  monitoring, managing and manipulating files 
@@ -48,7 +50,7 @@ fmInterface() {
 	clear
 	showheader
 	echo "File Manager Interface"
-	userInput
+	mainMenu
 }
 
 # Header
@@ -59,39 +61,76 @@ showheader() {
 	echo -e  "\___ | / /  \___ || | ${GREY}Time: ${RESET} $(date +%T)"
 	echo -e "${BLUE}\____//_/   \____/\_/ ${GREY}Date: ${RESET} $(date +%D)"
 	echo -e "${BLUE}++++++++++++++++++++++++++++"
+
+}
+
+commandMode() {
+	clear
+	showheader
+	echo -e "${RED}COMMAND MODE${RESET}\n$(pwd)"
+	echo -e "${BLUE}++++++++++++++++++++++++++++ ${RESET}"
+	
+	while true; do
+		echo -en "$(whoami)${RED}:CM${RESET} > "
+		read user_input
+
+		if [ "$user_input" = "quit" ] || [ "$user_input" = "exit" ]; then
+			mainMenu
+	      	elif [ "$user_input" = "quit!" ] || [ "$user_input" = "exit!" ]; then
+		      	exit
+		elif [ "$user_input" = "clear" ] || [ "$user_input" = "cls" ]; then
+		      	clear
+			commandMode
+		else 
+			eval $user_input
+		fi
+	done
+}
+
+
+# Main menu
+mainMenu() {
+	clear
+	showheader
 	kernel_version=$(uname -r)
 	os_name=$(grep -w "NAME=" /etc/os-release | cut -d'"' -f2)
 	os_version=$(grep -w "VERSION=" /etc/os-release | cut -d'"' -f2)
 	os_info="$os_name $os_version"
-	echo -e "${GREY}Operating system: ${Reset}$os_info ${GREY}Kernel Version: ${RESET}$kernel_version"
+	echo -e "${GREY}Operating system: ${RESET}$os_info ${GREY}Kernel Version: ${RESET}$kernel_version\n$(pwd)"
 	echo -e "${BLUE}++++++++++++++++++++++++++++ ${RESET}"
-}
 
-# User input while TRUE
-userInput() {
-	clear
-	showheader
 	while true; do
 		read -p "$(whoami) > " user_input
-	
-		case "$user_input" in
-			user|users|login|logins)
-			userInterface
-			;;
-			hardware|"check hardware"|specs|hw)
-			hardwareInterface
-			;;
-			files|"file management"|fm)
-			fmInterface
-			;;
-			exit|quit)
-			echo Ciao Cacao!
-			exit
-			;;
-			*)
-			echo -e "Unkown command. Try \"help\" to see all available commands.  "
-		esac
+		read -r first rest <<< $user_input
+		if [ "$first" = "CMD" ]; then
+			eval $rest	
+		else 
+			case "$user_input" in
+				user|users|login|logins)
+				userInterface
+				;;
+				hardware|"check hardware"|specs|hw)
+				hardwareInterface
+				;;
+				files|"file management"|fm)
+				fmInterface
+				;;
+				"CM!"|"command mode")
+				commandMode
+				;;
+				exit|quit)
+				echo Ciao Cacao!
+				exit
+				;;
+				clear|cls)	
+				clear
+				mainMenu
+				;;
+				*)
+				echo -e "${RED}Unkown command. Try \"help\" to see all available commands.${RESET}"
+			esac
+		fi
 	done	
 }
 
-userInput
+mainMenu
